bf98fa3497fd7736d4b77404f4fe3d304ab02d8e
Dylan <macyvesdylan@gmail.com>
2022-05-17 20:25:00 +0700

    remove aos

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
 import Head from "next/head";
 import Script from "next/script";
-import Aos from "aos";
-import "aos/dist/aos.css";
+// import Aos from "aos";
+// import "aos/dist/aos.css";
 import {
     MainSection,
     AboutSection,
@@ -12,9 +12,9 @@ import {
 } from "../components/index";
 
 export default function Home() {
-    useEffect(() => {
-        Aos.init({ duration: 2000 });
-    }, []);
+    // useEffect(() => {
+    //     Aos.init({ duration: 2000 });
+    // }, []);
     return (
         <React.Fragment>
             <div>
